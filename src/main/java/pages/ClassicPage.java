c97745b0225916988727547077040c11872d2615
Shobhit <55709498+shobhit19@users.noreply.github.com>
2025-01-13 14:04:23 +0530

    Create ClassicPage.java

---
+++ src/main/java/pages/ClassicPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class ClassicPage {
+
+    public static void main(String[] args) {
+
+        int aD2 = 10;
+        int bD2 = 20;
+        System.out.println(aD2+bD2);
+    }
+}
